3f7d97f2f993d01a1af3f47cd0d64d1c7d82f0f4
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-08 14:34:13 -0700

    added addons:docs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ exports.topic = {
 
 exports.commands = [
     require('./commands/addons'),
+    require('./commands/addons/docs'),
     require('./commands/addons/info'),
     require('./commands/addons/open'),
     require('./commands/addons/plans'),
