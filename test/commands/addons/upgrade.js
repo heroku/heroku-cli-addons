65558b098453de51f6ae8db28b93d1537952e30d
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-20 09:37:16 -0700

    fixed test suite

--- test/commands/addons/upgrade.js
+++ test/commands/addons/upgrade.js
@@ -31,17 +31,11 @@ describe('addons:upgrade', () => {
   });
 
   it('errors with no plan', () => {
-    return cmd.run({app: 'myapp', args: {addon: 'heroku-redis'}})
-    .then(() => expect(cli.stdout, 'to be empty'))
-    .then(() => expect(cli.stderr, 'to equal', ` ▸    Error: No plan specified.
- ▸    You need to specify a plan to move heroku-redis to.
- ▸    For example: heroku addons:upgrade heroku-redis:premium-0
- ▸    
- ▸    https://devcenter.heroku.com/articles/managing-add-ons
-`));
+    return expect(cmd.run({app: 'myapp', args: {addon: 'heroku-redis'}})
+                  , 'to be rejected with', /Error: No plan specified/);
   });
 
-  it('errors with no plan', () => {
+  it('errors with invalid plan', () => {
     let api = nock('https://api.heroku.com:443')
       .get('/addons/heroku-redis').reply(200, addon)
       .get('/addon-services/heroku-redis/plans').reply(200, [
@@ -50,40 +44,24 @@ describe('addons:upgrade', () => {
       ])
       .patch('/apps/myapp/addons/redis-swiftly-123', {plan: {name: 'heroku-redis:invalid'}})
       .reply(422, {message: 'Couldn\'t find either the add-on service or the add-on plan of "heroku-redis:invalid".'});
-    return cmd.run({app: 'myapp', args: {addon: 'heroku-redis:invalid'}})
-    .then(() => expect(cli.stdout, 'to be empty'))
-    .then(() => expect(cli.stderr, 'to equal', `Changing redis-swiftly-123 on myapp from premium-0 to heroku-redis:invalid... !!!
- ▸    Couldn't find either the add-on service or the add-on plan of
- ▸    "heroku-redis:invalid".
- ▸    
- ▸    Here are the available plans for heroku-redis:
- ▸    heroku-redis:free
- ▸    heroku-redis:premium-0
- ▸    
- ▸    See more plan information with heroku addons:plans heroku-redis
- ▸    
- ▸    https://devcenter.heroku.com/articles/managing-add-ons
-`))
+    return expect(cmd.run({app: 'myapp', args: {addon: 'heroku-redis:invalid'}})
+                  , 'to be rejected with', `Couldn't find either the add-on service or the add-on plan of "heroku-redis:invalid".
+
+Here are the available plans for heroku-redis:
+heroku-redis:free
+heroku-redis:premium-0
+
+See more plan information with heroku addons:plans heroku-redis
+
+https://devcenter.heroku.com/articles/managing-add-ons`)
 .then(() => api.done());
   });
 
   it('handles multiple add-ons', () => {
     let api = nock('https://api.heroku.com:443')
       .get('/addons/heroku-redis').reply(422, {id: 'multiple_matches', message: 'Ambiguous identifier; multiple matching add-ons found: redis-defined-2951, redis-rigid-2920.'});
-    return cmd.run({args: {addon: 'heroku-redis:invalid'}})
-    .then(() => expect(cli.stdout, 'to be empty'))
-    .then(() => expect(cli.stderr, 'to equal', ` ▸    Ambiguous identifier; multiple matching add-ons found: redis-defined-2951,
- ▸    redis-rigid-2920.
- ▸    
- ▸    Multiple add-ons match heroku-redis
- ▸    It is not clear which add-on's plan you are trying to change.
- ▸    
- ▸    Specify the add-on name instead of the name of the add-on service.
- ▸    For example, instead of: heroku addons:upgrade heroku-redis:invalid
- ▸    Run this: heroku addons:upgrade redis-triangular-1234 heroku-redis:invalid
- ▸    Alternatively, specify an app to filter by with --app
- ▸    https://devcenter.heroku.com/articles/managing-add-ons
-`))
-.then(() => api.done());
+    return expect(cmd.run({args: {addon: 'heroku-redis:invalid'}}),
+           'to be rejected with', /multiple matching add-ons found/)
+           .then(() => api.done());
   });
 });
--- test/init.js
+++ test/init.js
@@ -1,9 +1,9 @@
 'use strict';
 
 global.cli = require('heroku-cli-util');
-cli.raiseErrors = true;
 global.commands = require('..').commands;
 global.expect   = require('chai').expect;
 global.nock     = require('nock');
+cli.raiseErrors = true;
 process.stdout.columns = 80;
 process.stderr.columns = 80;
