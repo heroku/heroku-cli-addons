68d94bf503487faec3ebeb66f1d98cc33b66d0ab
Ransom Briggs <rbriggs@heroku.com>
2016-05-19 13:25:29 -0500

    Add test for fallback to global when app addon 404

--- test/commands/addons/info.js
+++ test/commands/addons/info.js
@@ -60,4 +60,31 @@ Price:        free
       })
     })
   })
+
+  context('with app but not an app add-on', function () {
+    beforeEach(function () {
+      nock('https://api.heroku.com', {reqheaders: {'Accept-Expansion': 'addon_service,plan'}})
+        .get('/apps/example/addons/www-db')
+        .reply(404)
+      nock('https://api.heroku.com', {reqheaders: {'Accept-Expansion': 'addon_service,plan'}})
+        .get('/addons/www-db')
+        .reply(200, fixtures.addons['www-db'])
+      nock('https://api.heroku.com')
+        .get(`/addons/${fixtures.addons['www-db']['id']}/addon-attachments`)
+        .reply(200, [fixtures.attachments['acme-inc-www::DATABASE']])
+    })
+
+    it('prints add-ons in a table', function () {
+      return cmd.run({flags: {}, args: {addon: 'www-db'}, app: 'example'}).then(function () {
+        util.expectOutput(cli.stdout,
+`=== www-db
+Attachments:  acme-inc-www::DATABASE
+Installed at: Invalid Date
+Owning app:   acme-inc-www
+Plan:         heroku-postgresql:hobby-dev
+Price:        free
+`)
+      })
+    })
+  })
 })
