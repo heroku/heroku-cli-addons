eae8a1cab70b007f80dcd328f4d84bc39fb90fc0
Jeff Dickey <dickeyxxx@gmail.com>
2016-06-30 14:57:43 -0700

    fix tests

--- test/lib/resolve.js
+++ test/lib/resolve.js
@@ -12,37 +12,37 @@ describe('resolve', () => {
   describe('addon', () => {
     it('finds a single matching addon', () => {
       let api = nock('https://api.heroku.com:443')
-        .get('/addons/myaddon').reply(200, {name: 'myaddon'})
+        .get('/addons/myaddon-1').reply(200, {name: 'myaddon-1'})
 
-      return resolve.addon(new Heroku(), null, 'myaddon')
-        .then((addon) => expect(addon, 'to satisfy', {name: 'myaddon'}))
+      return resolve.addon(new Heroku(), null, 'myaddon-1')
+        .then((addon) => expect(addon, 'to satisfy', {name: 'myaddon-1'}))
         .then(() => api.done())
     })
 
     it('finds a single matching addon for an app', () => {
       let api = nock('https://api.heroku.com:443')
-        .get('/apps/myapp/addons/myaddon').reply(200, {name: 'myaddon'})
+        .get('/apps/myapp/addons/myaddon-2').reply(200, {name: 'myaddon-2'})
 
-      return resolve.addon(new Heroku(), 'myapp', 'myaddon')
-        .then((addon) => expect(addon, 'to satisfy', {name: 'myaddon'}))
+      return resolve.addon(new Heroku(), 'myapp', 'myaddon-2')
+        .then((addon) => expect(addon, 'to satisfy', {name: 'myaddon-2'}))
         .then(() => api.done())
     })
 
     it('fails if no addon found', () => {
       nock('https://api.heroku.com:443')
-        .get('/apps/myapp/addons/myaddon').reply(404)
-        .get('/addons/myaddon').reply(404)
+        .get('/apps/myapp/addons/myaddon-3').reply(404)
+        .get('/addons/myaddon-3').reply(404)
 
-      return resolve.addon(new Heroku(), 'myapp', 'myaddon')
+      return resolve.addon(new Heroku(), 'myapp', 'myaddon-3')
         .then(() => { throw new Error('unreachable') })
         .catch((err) => expect(err, 'to satisfy', {statusCode: 404}))
     })
 
     it('fails if errored', () => {
       nock('https://api.heroku.com:443')
-        .get('/apps/myapp/addons/myaddon').reply(401)
+        .get('/apps/myapp/addons/myaddon-5').reply(401)
 
-      return resolve.addon(new Heroku(), 'myapp', 'myaddon')
+      return resolve.addon(new Heroku(), 'myapp', 'myaddon-5')
         .then(() => { throw new Error('unreachable') })
         .catch((err) => expect(err, 'to satisfy', {statusCode: 401}))
     })
@@ -60,10 +60,10 @@ describe('resolve', () => {
 
     it('finds a single matching attachment for an app', () => {
       let api = nock('https://api.heroku.com:443')
-        .get('/apps/myapp/addon-attachments/myattachment').reply(200, {name: 'myattachment'})
+        .get('/apps/myapp/addon-attachments/myattachment-1').reply(200, {name: 'myattachment-1'})
 
-      return resolve.attachment(new Heroku(), 'myapp', 'myattachment')
-        .then((addon) => expect(addon, 'to satisfy', {name: 'myattachment'}))
+      return resolve.attachment(new Heroku(), 'myapp', 'myattachment-1')
+        .then((addon) => expect(addon, 'to satisfy', {name: 'myattachment-1'}))
         .then(() => api.done())
     })
 
@@ -79,9 +79,9 @@ describe('resolve', () => {
 
     it('passes on errors getting app/attachment', () => {
       let api = nock('https://api.heroku.com:443')
-        .get('/apps/myapp/addon-attachments/myattachment').reply(401)
+        .get('/apps/myapp/addon-attachments/myattachment-2').reply(401)
 
-      return resolve.attachment(new Heroku(), 'myapp', 'myattachment')
+      return resolve.attachment(new Heroku(), 'myapp', 'myattachment-2')
         .then(() => { throw new Error('unreachable') })
         .catch((err) => expect(err, 'to satisfy', {statusCode: 401}))
         .then(() => api.done())
